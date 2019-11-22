c5ff310c49298d8e3c88c0ebc91b109dd44ccee0
Dan <ursu.andrei1999@gmail.com>
2019-11-22 14:36:02 +0200

    Main class modified a little bit for commit testing

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hei there, rrrrr");
+        System.out.println("hei there, rrrr, this line is a little bit different now");
+        System.out.println("added this line");
     }
 }
