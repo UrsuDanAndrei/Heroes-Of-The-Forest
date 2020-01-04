4380c6061c22cd262c1407560870102d5879f0ef
Dan <ursu.andrei1999@gmail.com>
2020-01-04 16:57:26 +0200

    Backup

--- src/output/GameOutputWriter.java
+++ src/output/GameOutputWriter.java
@@ -5,6 +5,7 @@ import heroes.Hero;
 import java.util.List;
 
 import fileio.FileSystem;
+import heroes.Pyromancer;
 
 public final class GameOutputWriter {
     private final String inputPath;
@@ -26,6 +27,7 @@ public final class GameOutputWriter {
         List<Hero> heroes = gameOutput.getHeroes();
         for (Hero hero : heroes) {
             try {
+
                 System.out.println(hero.toString());
                 fs.writeWord(hero.toString());
                 fs.writeNewLine();
